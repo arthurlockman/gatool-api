7db533dcdb5a04e6b9b1178e400dea4c19c8dce0
Arthur Rosa <arthur@rosafamily.net>
2022-08-28 17:48:47 -0700

    Updating max request size

--- app.js
+++ app.js
@@ -20,8 +20,8 @@ app.use(auth({
 
 }))
 app.use(morgan('dev'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(express.json({ limit: '50mb' }))
+app.use(express.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }))
 
 app.use((_, res, next) => {
   res.append('Access-Control-Allow-Origin', ['*'])
