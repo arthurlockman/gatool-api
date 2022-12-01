2ea1aa93df47dfb3a71c89ffc05bf81e03d5ae37
Arthur Rosa <arthur@rosafamily.net>
2022-11-30 20:48:05 -0800

    Removing accidental version cache

--- app.js
+++ app.js
@@ -39,10 +39,12 @@ app.use(express.json({ limit: '50mb' }))
 app.use(express.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }))
 
 app.get('/livecheck', (_, res) => {
+  res.setHeader('Cache-Control', 'no-cache')
   res.json({ message: `I'm alive!` })
 })
 
 app.get('/version', (_, res) => {
+  res.setHeader('Cache-Control', 'no-cache')
   res.json({ sha: appVersion })
 })
 
