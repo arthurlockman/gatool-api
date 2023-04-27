b5483ae1a905a9bf9d2e43ecc13e51cef414d03a
James Lockman <jlockman@adobe.com>
2023-04-27 10:08:42 -0400

    last try

--- routes/v3-endpoints.js
+++ routes/v3-endpoints.js
@@ -478,15 +478,11 @@ const BuildHybridSchedule = async (year, eventCode, tournamentLevel) => {
     }
     const schedule = scheduleResponse.body.schedule || scheduleResponse.body.Schedule
     const matches = matchesResponse.body.matches || matchesResponse.body.Matches
+    const headers = {schedule:scheduleResponse.headers,matches:matchesResponse.headers}
 
     _.merge(schedule, matches)
 
-    schedule.headers = {
-        "schedule": scheduleResponse.get("Last-Modified"),
-        "matches": matchesResponse.get("Last-Modified")
-    }
-
-    return schedule
+    return {schedule: schedule,headers:headers}
 }
 
 export const UpdateHighScores = async () => {
